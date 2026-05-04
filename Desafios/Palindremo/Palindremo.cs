081451623232deceea69ed935458f3383a02f319
Esdras Lima <esdras.lima@aevo.com.br>
2026-05-04 20:51:26 -0300

    feat(palindromo): adiciona desafio do palindromo

---
+++ Desafios/Palindremo/Palindremo.cs
@@ -0,0 +1,33 @@
+﻿public static class Palindromo
+{
+    public static bool EhPalindromo(string texto)
+    {
+        if (string.IsNullOrWhiteSpace(texto)) return false;
+
+        var textoLimpo = LimparTexto(texto);
+
+        int inicio = 0;
+        int fim = textoLimpo.Length - 1;
+
+        while (inicio < fim)
+        {
+            if (textoLimpo[inicio] != textoLimpo[fim]) return false;
+            inicio++;
+            fim--;
+        }
+
+        return true;
+    }
+
+    private static string LimparTexto(string texto)
+    {
+        var resultado = "";
+
+        foreach (char c in texto)
+        {
+            if (char.IsLetterOrDigit(c)) resultado += char.ToLower(c);
+        }
+
+        return resultado;
+    }
+}
---
+++ Desafios/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// Desafio do Palindromo
+
+string texto1 = "Ovo";
+string texto2 = "Roma me tem amor";
+string texto3 = "O lobo ama o bolo";
+string texto4 = "Não sou um palindromo";
+
+Console.WriteLine("Desafio do Palindromo");
+Console.WriteLine(texto1 + ": " + Palindromo.EhPalindromo(texto1));
+Console.WriteLine(texto2 + ": " + Palindromo.EhPalindromo(texto2));
+Console.WriteLine(texto3 + ": " + Palindromo.EhPalindromo(texto3)); 
+Console.WriteLine(texto4 + ": " + Palindromo.EhPalindromo(texto4)); 
+Console.WriteLine("-------------------------------------------------");
